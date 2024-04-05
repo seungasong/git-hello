40fc9352dcebfca38f8d9f716d8a800a47d8ae47
seungasong <ssa9706@gmail.com>
2024-04-05 09:47:05 +0900

    1st Commit

---
+++ src/com/ezen/git/HelloGitHub.java
@@ -0,0 +1,9 @@
+package com.ezen.git;
+
+public class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("111111");
+        System.out.println("222222");
+        System.out.println("333333");
+    }
+}
---
+++ src/com/ezen/git/some.java
@@ -0,0 +1,4 @@
+package com.ezen.git;
+
+public class some {
+}
